e4e3658e6393f5491543693fe31ac13cf5b53329
Nil Bagaria Nofre <nil.bagaria@students.salle.url.edu>
2023-06-11 12:27:41 +0200

    Commit

--- app/src/main/java/com/example/coolgifts/LoginActivity.java
+++ app/src/main/java/com/example/coolgifts/LoginActivity.java
@@ -4,19 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextView forgotPassword;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginBtn;
+    private TextView forgotPassword;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        emailEditText = findViewById(R.id.et_mail);
+        passwordEditText = findViewById(R.id.et_password);
+        loginBtn = findViewById(R.id.btn_login);
         forgotPassword = findViewById(R.id.forgotPassword);
 
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                //TODO: Lógica de inicio de sesión.
+
+                Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+            }
+        });
         forgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
