06b221e7b4d5310d4c4f46cdf3549fea8b1faa2c
roger27012003 <126463415+roger27012003@users.noreply.github.com>
2023-06-13 17:29:47 +0200

    Registro acabado, login pendiente

---
+++ app/src/main/java/com/example/coolgifts/api/PerfilActivity.java
@@ -0,0 +1,26 @@
+package com.example.coolgifts.api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.coolgifts.BasicActivity;
+import com.example.coolgifts.MainActivity;
+import com.example.coolgifts.MenuActivity;
+import com.example.coolgifts.R;
+
+public class PerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+    }
+    public void onClickHome(View view) {
+        Intent MenuIntent = new Intent(this, MenuActivity.class);
+        startActivity(MenuIntent);
+    }
+
+}
