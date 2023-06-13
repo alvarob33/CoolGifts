f4b0b5b92a0ead131675770fdf295183745d3a68
Nil Bagaria Nofre <nil.bagaria@students.salle.url.edu>
2023-06-13 21:15:06 +0200

    Activity friends Activity WishList Configuració del menu 'onclick' -> Activities Friends i WishLists

--- app/src/main/java/com/example/coolgifts/MenuActivity.java
+++ app/src/main/java/com/example/coolgifts/MenuActivity.java
@@ -2,7 +2,9 @@ package com.example.coolgifts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -11,4 +13,43 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
+
+
+    public void onImageButtonFriendsClick(View view) {
+        // Quan es selecciona la opcio Amistats (Friends)
+        Intent intent = new Intent(MenuActivity.this, FriendsActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void onImageButtonGiftClick(View view) {
+        // Quan es selecciona la opcio Regals (WishList)
+        Intent intent = new Intent(MenuActivity.this, WishListActivity.class);
+        startActivity(intent);
+    }
+
+    //TODO: Descommentar les funcions un cop s'hagin creat les activities corresponents i
+    // s'hagin assignat dins de Intent on hi ha 'Activity1.class'.
+
+    /*
+    public void onImageButtonIconoUsuarioClick(View view) {
+        // Quan es selecciona la opcio Perfil (IconoUsuario)
+        Intent intent = new Intent(MenuActivity.this, Activity1.class); //TODO: Modificar l'activity corresponent a la del Perfil d'usuari
+        startActivity(intent);
+    }
+
+    public void onImageButtonMessageClick(View view) {
+        // Quan es selecciona la opcio Missatges (Message)
+        Intent intent = new Intent(MenuActivity.this, Activity1.class); //TODO: Modificar l'activity corresponent a la de Missatges
+        startActivity(intent);
+    }
+
+    public void onImageButtonSearchClick(View view) {
+        // Quan es selecciona la opcio Cercar (Search)
+        Intent intent = new Intent(MenuActivity.this, Activity1.class); //TODO: Modificar l'activity corresponent a la de Buscar
+        startActivity(intent);
+    }
+
+     */
+
 }
